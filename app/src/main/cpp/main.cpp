e1f63d2aecc3f7ffb4f41750ce36d0be32faa71e
Paul "TBBle" Hampson <Paul.Hampson@Pobox.com>
2018-09-24 01:26:11 +0900

    Implement bare-minimum main loop for native_app_glue
    This loops until the application is destroyed, and ensures events
    are passed back up to native_app_glue for default processing and
    app lifecycle control.

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -1,10 +1,22 @@
 #include <android_native_app_glue.h>
 
+#include <android/looper.h>
+
 /**
  * Entry point for the application thread
  */
+void android_main(struct android_app *pApp) {
+    // Absolute bare-minimum application loop for native_app_glue
+    // Assumes any non-callback-based events added to the Looper will use a
+    // struct android_poll_source* as their data pointer.
+    while (1) {
+        struct android_poll_source *pSource = nullptr;
+        if (ALooper_pollAll(-1, nullptr, nullptr, reinterpret_cast<void **>(&pSource)) > 0) {
+            pSource->process(pSource->app, pSource);
+        }
 
-void android_main(struct android_app* app)
-{
-    // Do nothing
+        if (pApp->destroyRequested) {
+            break;
+        }
+    }
 }
